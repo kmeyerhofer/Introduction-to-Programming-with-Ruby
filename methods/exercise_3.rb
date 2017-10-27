1f002cefc3ee351b0817a2ecdfb6e05d3e083de9
kmeyerhofer <k@kcmr.io>
2017-10-27 16:40:42 -0700

    Exercises from Methods Chapter

---
+++ methods/exercise_3.rb
@@ -0,0 +1,5 @@
+def multiply(one, two)
+  one * two
+end
+
+puts multiply(4, 6)
---
+++ methods/exercise_4.rb
@@ -0,0 +1,9 @@
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+
+#Printed to the screen will be nothing, returning nil.
