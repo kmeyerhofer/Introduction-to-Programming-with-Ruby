0d327750a64fa2fa8558535cada098a7bdf7b7c2
kmeyerhofer <k@kcmr.io>
2017-10-27 16:13:36 -0700

    Finishes exercises from Variables Chapter

---
+++ variables/exercise_5.md
@@ -0,0 +1,18 @@
+x = 0
+3.times do
+  x += 1
+end
+puts x
+
+
+x is equal to 3.
+
+
+y = 0
+3.times do
+  y += 1
+  x = y
+end
+puts x
+
+x is outside of the scope of the block and produces an error.
--- variables/name.rb
+++ variables/name.rb
@@ -1,5 +1,7 @@
-print "Type your name here >> "
-name = gets.chomp
-puts "Greetings #{name}!"
+print "Type your first name here >> "
+first_name = gets.chomp
+print "Type your last name here >> "
+last_name = gets.chomp
+puts "Greetings #{first_name} #{last_name}!"
 
-10.times {puts name}
+10.times {puts "#{first_name} #{last_name}"}
